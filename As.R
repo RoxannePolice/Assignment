8feb816bb94e20ff43d917e7554de0229beb14d4
RoxannePolice <creep19t93@gmail.com>
2015-08-24 01:11:49 +0200

    Update As.R

--- As.R
+++ As.R
@@ -1,3 +1,6 @@
+##These two functions should create a matrix, which could later be cached and inverted.
+
+##The result of this first function should be a cached matrix. The inverted matrix should also be cached.
 makeCacheMatrix <- function(x = matrix()) {
 m<-NULL
 set<-function(y) {
@@ -11,7 +14,9 @@ setmean=setmean,
 getmean=getmean)
 }
 
-cacheSolve <- function (x, ...) {
+##The second function should return only the already existing inverted matrix. Should the inverted matrix not exist, 
+## the function should create one.
+cacheSolve <- function (x, ...) { 
 s<-x$getsolve()
 if(!is.null(s)) {
 	message("getting cached data")
